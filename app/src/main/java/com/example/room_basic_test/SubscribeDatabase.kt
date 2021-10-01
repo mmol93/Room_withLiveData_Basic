6510158c6e3bd9dd0dcc7db4bb19c005bc7e7cbf
Ljw <ljws93@naver.com>
2021-10-01 13:15:53 +0900

    make room database

---
+++ app/src/main/java/com/example/room_basic_test/SubscribeDatabase.kt
@@ -0,0 +1,36 @@
+package com.example.room_basic_test
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// 어떤 데이터를 참고하여 DB를 만들지, 버전은 뭐로할지 정의
+@Database(entities = [Subscriber::class], version = 1)
+abstract class SubscriberDatabase : RoomDatabase(){
+    abstract val subscriberDAO : SubscriberDAO
+
+    companion object{
+        // Volatile: 공유되는 변수에서 사용되면 주로 멀티 스레드 처리에 사용된다
+        // 스레드에서 엑세스 할 때마다 반드시 공유 메모리상의 변수의 값과 스레드 값을 일치시킨다
+        // 즉, 다른 스레드에서 참조할 수 있는 변수임
+        @Volatile
+        private var INSTANCE : SubscriberDatabase? = null
+        // 여기 아랫부분은 모든 Room 라이브러리를 쓸 때 공통적으로 쓰는 부분이다
+        // 다른 프로젝트에서 쓸 때는 클래스 이름(SubscriberDatabase::class.java)과
+        // DB 이름만 바꿔서 사용하면된다
+        fun getInstance(context: Context) : SubscriberDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SubscriberDatabase::class.java,
+                        "subscriber_data_database"
+                    ).build()
+                }
+                return instance
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/room_basic_test/SubscriberDAO.kt
@@ -0,0 +1,33 @@
+package com.example.room_basic_test
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+interface SubscriberDAO {
+    // @Insert: 해당 정보(테이블, 컬러명 등)로 DB를 만든다
+    // Room은 main 스레드에 있는 데이터 베이스에 접근할 수 없다
+    // 그래서 suspend fun를 이용해서 background에서 작동하게 한다
+    // OnConflictStrategy.REPLACE: 해당 인덱스에 이미 값이 있을 경우 replace 한다
+    // Long 반환을 사욯하면 rowId를 반환받을 수 있다
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubscriber(subscriber: Subscriber) : Long
+
+    // 구독자 데이터 업데이트
+    @Update
+    suspend fun updateSubscriber(subscriber: Subscriber)
+
+    // 구독자 데이터 삭제
+    @Delete
+    suspend fun deleteSubscriber(subscriber: Subscriber)
+
+    // 구독자 데이터 전부 삭제
+    @Query("Delete FROM subscriber_data_table")
+    suspend fun clearSubscribers()
+
+    // 구독자 데이터 전부 읽기
+    // Room2.2부터 코루틴의 FLOW 사용가능
+    // FLOW 사용시 함수 호출은 메인 스레드에서, 데이터 처리는 백그라운드에서 가능하다
+    // 호출을 메인 스레드에서 하기 때문에 그냥 fun
+    @Query("SELECT * FROM subscriber_data_table")
+    fun getAllSubscriber() : Flow<List<Subscriber>>
+}
